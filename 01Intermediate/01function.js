186d38345454bf89b91cefe5080158e2434869fd
Rabindra Mahato <robinmanohar@gmail.com>
2019-12-26 10:38:51 +0530

    function Intermediate level just started

---
+++ 01Intermediate/01function.js
@@ -0,0 +1,13 @@
+let sayHello = function(name) {
+    console.log('Hello first function!');
+    console.log(`preety cool!! ${name}`);
+}
+
+sayHello('Jhone');
+
+let fullNameMakers = function(firstName, lastName) {
+    console.log("Hey! welcome buddy.");
+    console.log(`Great to you here Mr. ${firstName} ${lastName}`)
+}
+
+fullNameMakers('Jhone', 'Doe')
